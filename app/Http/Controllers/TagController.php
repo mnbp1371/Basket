e5361c0ff8bbd5087dbbd4a5ea15f8e55664de53
mohammadnabipour <mohammadnabipour1371@gmail.com>
2018-11-22 21:54:13 +0330

    added store tag and index tags

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Tag;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
-    //
+
+    public function store(Request $request){
+
+        $tag= new Tag();
+        $tag->name=$request->tagname;
+        $tag->save();
+        return back();
+
+    }
+
+    public function index(Tag $tag){
+
+        $posts=$tag->post;
+        return view('post',compact('posts'));
+
+    }
 }
