9681b22dd8e9b438e29bbef2dd58736316d2ccfb
mohammadnabipour <mohammadnabipour1371@gmail.com>
2018-12-01 17:37:15 +0330

    added basket model view controller

---
+++ database/migrations/2018_12_01_160057_create_baskets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBasketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('baskets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('user_id');
+            $table->text('cart');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('baskets');
+    }
+}
